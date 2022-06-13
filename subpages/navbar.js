ea66ad9d335bdddc6cc56bf3c563a9d720a59673
zanzan13531 <zanzan13531@gmail.com>
2022-06-12 19:07:43 -0700

    added a slightly differently configured navbar for the subpages

--- subpages/navbar.js
+++ subpages/navbar.js
@@ -1,7 +1,7 @@
 document.write('<nav class="navbar navbar-inverse">\
 <div class="container-fluid">\
   <div class="navbar-header">\
-    <a class="navbar-brand" href="index.html">chemCalculator</a>\
+    <a class="navbar-brand" href="../index.html">chemCalculator</a>\
   </div>\
   <ul class="nav navbar-nav">\
     <!-- <li class="active"><a href="#">Home</a></li> --> \
@@ -9,9 +9,9 @@ document.write('<nav class="navbar navbar-inverse">\
       <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1\
       <span class="caret"></span></a>\
       <ul class="dropdown-menu">\
-        <li><a href="subpages/topic_a.html">Topic A</a></li>\
-        <li><a href="subpages/topic_b.html">Topic B</a></li>\
-        <li><a href="subpages/topic_c.html">Topic C</a></li>\
+        <li><a href="topic_a.html">Topic A</a></li>\
+        <li><a href="topic_b.html">Topic B</a></li>\
+        <li><a href="topic_c.html">Topic C</a></li>\
       </ul>\
     </li>\
     <li><a href="#">Page 2</a></li>\
