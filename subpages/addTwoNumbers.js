1d3882938c4810d08fd615602fef804e3315c621
zanzan13531 <zanzan13531@gmail.com>
2022-06-14 15:13:13 -0700

    trying to add in bignumber stuff

--- subpages/addTwoNumbers.js
+++ subpages/addTwoNumbers.js
@@ -1,17 +1,19 @@
+import BigNumber from "../bignumber/bignumber.js";
+
 function addTwoNumbers() {
 
     var number1 = document.getElementById('numberField1').value;
     var number2 = document.getElementById('numberField2').value;
 
-    number1 = Number(number1);
-    number2 = Number(number2);
+    number1 = new BigNumber(number1);
+    number2 = new BigNumber(number2);
 
     var resultBox = document.getElementById('result');
 
     if ((number1 == NaN) || (number2 == NaN)) {
         resultBox.innerHTML = "Invalid Input";
     } else {
-        resultBox.innerHTML = number1 + number2;
+        resultBox.innerHTML = new BigNumber(number1.minus(number2)).toString();
     }
 
 }
