9ab7f1a623666c5eafbe507e3d64a3f251804707
zanzan13531 <zanzan13531@gmail.com>
2022-06-20 15:27:50 -0700

    changed from log(base, number) to log10(number)

--- subpages/hPlusToPh.js
+++ subpages/hPlusToPh.js
@@ -9,10 +9,7 @@ function hPlusToPh() {
     if (number1 == NaN) {
         resultBox.innerHTML = "Invalid Input";
     } else {
-        console.log(number1.toNumber());
-        console.log(Math.log(10, number1.toNumber()));
-        console.log(-Math.log(10, number1.toNumber()));
-        resultBox.innerHTML = -Math.log(10, number1.toNumber());
+        resultBox.innerHTML = -Math.log10(number1.toNumber());
     }
 
 }
